5bc476d29ce0ae544cd8cd17c927d8eeb37b8c63
Tim Finholm <legendarytimfinholm@gmail.com>
2015-05-24 15:28:00 -0400

    did the assignment, and put the answer in there

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: tim finholm
+## makeCacheMatrix builds the cache matrix from user-supplied input and sets the inverse to null, to avoid using a cached value later
+## cacheSolve gets the inverse of the matrix and returns it
 
-## Write a short comment describing this function
-
+## this function builds the matrix (take the red pill)
 makeCacheMatrix <- function(x = matrix()) {
-
+    if(!is.matrix(x)){     # test that x is a matrix
+    	message("x is not a matrix, exiting")
+    	stop
+    }
+    inverse <- NULL         # start with a null inverse
+    matrix <- function(y) { 
+        x <<- y             # let x take y
+        inverse <<- NULL    # and re-set the inverse to null
+    }
+    get <- function() x     # get x
+    setinverse <- function(flip) inverse <<- flip  # set the inverse
+    getinverse <- function() inverse               # get the inverse
+    list(matrix=matrix, get=get, setinverse=setinverse, getinverse=getinverse) # return a list
 }
 
-
-## Write a short comment describing this function
-
+# solve the cache if necessary
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()  # get the inverse
+    if(!is.null(inverse)) {    # don't solve anything if the cached data is still valid (inverse would be set to null if the matrix is new) 
+        message("using cached data.") # let the user know cached data is being used
+        return(inverse)        # return the inverse
+    }
+    else {                     # if the inverse is null, we have work to do
+    	values <- x$get()         # get the values
+    	inverse <- solve(values)  # solve the matrix
+    	x$setinverse(inverse)     # set the inverse for later use if possible
+    	return(inverse)           # return the inverse to the calling function
+    	}
 }
